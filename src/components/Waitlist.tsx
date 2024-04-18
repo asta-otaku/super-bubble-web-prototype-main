cb219a48f8a2cf7ba5b01fd01f7f02b7850486eb
asta-otaku <afolabiibrahim08@gmail.com>
2024-04-18 18:49:57 +0100

    fix: UI fixes

--- src/components/Waitlist.tsx
+++ src/components/Waitlist.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import back from "../assets/back.svg";
-import subtract from "../assets/Subtract.svg";
+// import subtract from "../assets/Subtract.svg";
 import { LeftIcon, RightIcon } from "../assets/icons";
 
 const questions = [
@@ -32,7 +32,7 @@ function Waitlist({
     >
       <button
         style={{
-          backgroundImage: `url(${subtract})`,
+          backgroundImage: `url(../assets/Subtract.svg)`,
         }}
         onClick={() => setOpen(!open)}
         className={`text-primary font-medium hover:text-secondary bg-cover bg-no-repeat bg-center w-[300px] h-16`}
